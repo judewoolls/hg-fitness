0aadeb8c7f6d6c5bdb55ee927ca64dc1b1542cca
Jude Woolls <jwoolls01@gmail.com>
2025-01-30 22:41:55 +0000

    add whitenoise

--- hgfitness/settings.py
+++ hgfitness/settings.py
@@ -70,6 +70,7 @@ LOGIN_URL = 'account_login'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
